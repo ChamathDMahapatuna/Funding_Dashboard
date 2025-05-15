17dba43fdbd2ede65528971d67dd28fee459e458
M.D.C.D.Mahapatuna <109390299+ChamathDMahapatuna@users.noreply.github.com>
2025-05-15 13:18:02 +0530

    remove search bar in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,7 @@
 // src/components/Header.jsx
 import { useContext } from 'react';
 import { 
-  BellIcon, 
-  QuestionMarkCircleIcon, 
+  BellIcon,
   UserCircleIcon 
 } from '@heroicons/react/24/outline';
 import { Link } from 'react-router-dom';
@@ -24,8 +23,6 @@ function Header() {
             </span>
           </div>
           
-          <QuestionMarkCircleIcon className="h-6 w-6 text-gray-500 hover:text-gray-700 cursor-pointer" />
-          
           <div className="flex items-center cursor-pointer">
             <UserCircleIcon className="h-8 w-8 text-blue-500" />
             <div className="ml-2 hidden md:block">
@@ -46,8 +43,6 @@ function Header() {
           </div>
         </div>
       </div>
-      
-    
     </header>
   );
 }
